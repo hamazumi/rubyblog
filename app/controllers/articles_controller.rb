4623230567c4861fe9253faf8f2d99d515d03cad
Ken Hamamoto <khamamoto12@gmail.com>
2021-03-30 11:45:51 -0700

    Updating an Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,15 +18,32 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render :new
-    end 
+    end
   end
 
-private
-  def article_params
-    params.require(:article).permit(:title, :body)
+  def edit
+    @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
-
-# adding comment 
-
-
